b8c378aae248da76d0c6ef2b65264e70391c5fb6
santosjared <santosjared221234@gmail.com>
2024-03-31 00:33:37 -0400

    delete src duplite

--- src/src/configs/environment.ts
+++
@@ -1,3 +0,0 @@
-export default ()=>({
-    backendURI: process.env.NEXT_PUBLIC_BACKEND_URI || 'http://localhost:3001'
-})
--- src/src/store/instancesAxios.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import getConfig from '../configs/environment'
-const axiosInstance = axios.create({
-  baseURL: getConfig().backendURI,
-  timeout: 1000
-});
-
-export default axiosInstance;
